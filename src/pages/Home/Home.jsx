9f06dd9de34256d6e04cb392ab3bf4c35d6076ff
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-05-11 23:19:47 +0200

    MAJ pages

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,9 @@ import React from "react";
 import About from "../../components/about/About";
 import Project from "../../components/project/Projet";
 
+// STYLE
+import "./home.css"
+
 function Home() {
   return (
     <>
