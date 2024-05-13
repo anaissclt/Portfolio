0f1138453669bb40c03d42efa480ea23fe5571ee
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-05-13 22:54:43 +0200

    Ajout composant skill

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import React from "react";
 // COMPOSANTS
 import About from "../../components/about/About";
 import Project from "../../components/project/Projet";
+import Skill from "../../components/skills/Skills"
 
 // STYLE
 import "./home.css"
@@ -13,6 +14,7 @@ function Home() {
     <>
       <About />
       <Project />
+      <Skill />
     </>
   );
 }
