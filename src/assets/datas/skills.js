277e30668ab2c64bceb4d656517aeffc3da97b5f
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-05-14 23:27:09 +0200

    Maj datas

---
+++ src/assets/datas/skills.js
@@ -0,0 +1,65 @@
+import logoReact from "../../assets/images/react.webp"
+import logoHtml from "../../assets/images/HTML.png"
+import logoJavascript from "../../assets/images/JavaScript.png"
+import logoCss from "../../assets/images/CSS.png"
+import logoBootstrap from "../../assets/images/Bootstrap.png"
+import logoRedux from "../../assets/images/Redux.png"
+import logoNotion from "../../assets/images/Notion.png"
+import logoTrello from "../../assets/images/Trello.png"
+import logoSass from "../../assets/images/Sass.png"
+import logoFigma from "../../assets/images/Figma.png"
+
+const skills = [
+  {
+    "nom": "React",
+    "alt": "React Logo",
+    "url": logoReact,
+  },
+  {
+    "nom": "Javascript",
+    "alt": "Javascript Logo",
+    "url": logoJavascript,
+  },
+  {
+    "nom": "HTML",
+    "alt": "HTML Logo",
+    "url": logoHtml,
+  },
+  {
+    "nom": "CSS",
+    "alt": "CSS Logo",
+    "url": logoCss,
+  },
+  {
+    "nom": "Redux",
+    "alt": "Redux Logo",
+    "url": logoRedux,
+  },
+  {
+    "nom": "Notion",
+    "alt": "Notion Logo",
+    "url": logoNotion,
+  },
+  {
+    "nom": "Trello",
+    "alt": "Trello Logo",
+    "url": logoTrello,
+  },
+  {
+    "nom": "SASS",
+    "alt": "SASS Logo",
+    "url": logoSass,
+  },
+  {
+    "nom": "Figma",
+    "alt": "Figma Logo",
+    "url": logoFigma,
+  },
+  {
+    "nom": "Bootstraps",
+    "alt": "Bootstraps Logo",
+    "url": logoBootstrap,
+  }
+];
+
+export default skills;
