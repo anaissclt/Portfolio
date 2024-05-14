1578b8dfe9a5bce10d5fc53623aff5ac46181073
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-05-14 23:28:38 +0200

    Maj responsive

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 // STYLE 
 import './skills.css'
 
 // DATAS 
-import SkillsData from '../../assets/datas/skills.json';
+import SkillsData from '../../assets/datas/skills';
 
 const Skill = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const scrollSkills = () => {
+      container.scrollLeft += 1; // Défilement d'un pixel à la fois
+
+      // revenir au début si on a dépassé la largeur du contenu 
+      if (container.scrollLeft >= container.scrollWidth / 2) {
+        container.scrollLeft = 0;
+      }
+    };
+
+    // Intervalle pour le défilement automatique
+    const scrollInterval = setInterval(scrollSkills, 5); // vitesse du défilement
+
+    return () => clearInterval(scrollInterval);
+  }, []);
+
+  // Dupliquer les éléments de manière dynamique
+  const duplicatedSkills = SkillsData.concat(SkillsData);
+  
   return (
     <div className='div-skills'>
-    <h1 className="skills-title-h1">Mes compétences</h1>
-    <div className="skills-container">
-      {SkillsData.projets.map((skills, index) => (
-        <div key={index} className="skills-item">
-          <h2 className="skills-title">{skills.nom}</h2>
-          <img src={skills.logo} alt={skills.nom} className="skills-logo" />
+      <h1 className="skills-title-h1">Mes compétences</h1>
+      <div className="skills-outer-container">
+        <div className="skills-container" ref={containerRef}>
+          {duplicatedSkills.map((skills, index) => (
+            <div key={index + SkillsData.length} className="skills-item">
+              <h2 className="skills-title">{skills.nom}</h2>
+              <img width={100} height={100} src={skills.url} alt={skills.nom} className="skills-logo" />
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
-  </div>
   );
-}
+};
 
 export default Skill;
