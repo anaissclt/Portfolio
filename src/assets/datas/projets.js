b44aec056257bf86b2bf6b1cf5cb0dd18794d1e0
anaissclt <71971929+anaissclt@users.noreply.github.com>
2024-05-15 18:26:36 +0200

    Ajout img alt

--- src/assets/datas/projets.js
+++ src/assets/datas/projets.js
@@ -16,56 +16,62 @@ const projets = [
         "lien": "https://github.com/anaissclt/OcBooki",
         "url": bookiImage,
         "alt": "Logo de Booki"
-
       },
       {
         "nom": "Ohmyfood",
         "description": "Amélioration d'une interface pour un site mobile avec des animations CSS.",
         "competences": ["Github", "Git", "Animations CSS"],
         "lien": "https://github.com/anaissclt/OCohmyfood",
-        "url": ohmyfoodImage
+        "url": ohmyfoodImage,
+        "alt": "Logo de Ohmyfood"
       },
       {
         "nom": "Portfolio - Sophie Bluel",
         "description": "Création d'une page web dynamique avec JavaScript.",
         "competences": ["JavaScript"],
         "lien": "https://github.com/anaissclt/OcSophieBluel",
-        "url": sophieImage
+        "url": sophieImage,
+        "alt": "Logo de Sophie Bluel"
       },
       {
         "nom": "Qwenta - Menu Maker ",
         "description": "Planification le développement du site de votre client.",
         "competences": ["Gestion de projet", "Veille technologique"],
         "lien": "",
-        "url": qwentaImage
+        "url": qwentaImage,
+        "alt": "Logo de Qwenta"
       },
       {
         "nom": "Kasa",
         "description": "Créez une application web de location immobilière avec React.",
         "competences": ["Create React App", "React Router"],
         "lien": "https://github.com/anaissclt/OcKasa",
-        "url": kasaImage
+        "url": kasaImage,
+        "alt": "Logo de Kasa"
       },
       {
         "nom": "Portfolio - Nina Carducci",
         "description": "Optimisez le référencement d'un site de photographe.",
         "competences": ["SEO"],
         "lien": "https://github.com/anaissclt/OcNinaCarducci",
-        "url": ninaImage
+        "url": ninaImage,
+        "alt": "Logo de Nina Carducci"
       },
       {
         "nom": "724events",
         "description": "Débuggez le site d'une agence d'événementiel.",
         "competences": ["Chrome DevTools"],
         "lien": "https://github.com/anaissclt/Oc724Events",
-        "url": eventsImage
+        "url": eventsImage,
+        "alt": "Logo de 724events"
       },
       {
         "nom": "Argent Bank",
         "description": "Implémentez le front-end d'une application bancaire avec React.",
         "competences": ["Communication client / serveur","Redux"],
         "lien": "https://github.com/anaissclt/OcArgentBank_Frontend",
-        "url": bankImage
+        "url": bankImage,
+        "alt": "Logo de Argent Bank"
       }
     ]
 
